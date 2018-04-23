87bbe2e2859a3279ec5760b9319a0a13d51ad208
hezhuohua <hzh081215@qq.com>
2018-04-23 22:46:14 +0800

    aaaaaaaaaaaaaaaaaaaaaaa

--- src/components/menu/header/BaseHeader.vue
+++ src/components/menu/header/BaseHeader.vue
@@ -1,28 +1,53 @@
 <template>
-<el-row type="flex" justify="center">
-  <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
+<div class="header">
+  <el-row type="flex" justify="center">
+  <el-col :span="6"><div class=""></div></el-col>
   <el-col :span="12">
-    <el-menu :default-active="activeIndex" class="el-menu" mode="horizontal" @select="handleSelect">
-  <el-menu-item index="1">处理中心</el-menu-item>
-  <el-submenu index="2">
-    <template slot="title">我的工作台</template>
-    <el-menu-item index="2-1">选项1</el-menu-item>
-    <el-menu-item index="2-2">选项2</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
-    <el-submenu index="2-4">
-      <template slot="title">选项4</template>
-      <el-menu-item index="2-4-1">选项1</el-menu-item>
-      <el-menu-item index="2-4-2">选项2</el-menu-item>
-      <el-menu-item index="2-4-3">选项3</el-menu-item>
-    </el-submenu>
-  </el-submenu>
-  <el-menu-item index="3" disabled>消息中心</el-menu-item>
-  <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-</el-menu>
+       <div class="container">
+
+            <ul id="nav">
+                <li><a href="http://www.script-tutorials.com/">Home</a></li>
+                <li><a class="hsubs" href="#">Menu 1</a>
+                    <ul class="subs">
+                        <li><a href="http://www.16sucai.com/">Submenu 1</a></li>
+                        <li><a href="#">Submenu 2</a></li>
+                        <li><a href="#">Submenu 3</a></li>
+                        <li><a href="#">Submenu 4</a></li>
+                        <li><a href="http://www.16sucai.com/">Submenu 5</a></li>
+                    </ul>
+                </li>
+                <li><a class="hsubs" href="#">Menu 2</a>
+                    <ul class="subs">
+                        <li><a href="#">Submenu 2-1</a></li>
+                        <li><a href="#">Submenu 2-2</a></li>
+                        <li><a href="#">Submenu 2-3</a></li>
+                        <li><a href="#">Submenu 2-4</a></li>
+                        <li><a href="#">Submenu 2-5</a></li>
+                        <li><a href="#">Submenu 2-6</a></li>
+                        <li><a href="#">Submenu 2-7</a></li>
+                        <li><a href="http://www.16sucai.com/">Submenu 2-8</a></li>
+                    </ul>
+                </li>
+                <li><a class="hsubs" href="#">Menu 3</a>
+                    <ul class="subs">
+                        <li><a href="#">Submenu 3-1</a></li>
+                        <li><a href="#">Submenu 3-2</a></li>
+                        <li><a href="#">Submenu 3-3</a></li>
+                        <li><a href="#">Submenu 3-4</a></li>
+                        <li><a href="http://www.16sucai.com/">Submenu 3-5</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Menu 4</a></li>
+                <li><a href="#">Menu 5</a></li>
+                <li><a href="#">Menu 6</a></li>
+                <li><a href="http://www.16sucai.com/">Back</a></li>
+                <div id="lavalamp"></div>
+            </ul>
+        </div>
   </el-col>
   <el-col :span="6"><div class="grid-content bg-purple"></div></el-col>
 </el-row>
-
+</div>
 </template>
 
 <script>
@@ -40,3 +65,20 @@ export default {
   }
 }
 </script>
+
+<style>
+@import './header.css';
+*{
+    margin:0;
+    padding:0;
+}
+.header{
+  width:100%;
+  height: 100%;
+  background-color:red;
+}
+.container {
+    height: 70%;
+    /* width: 957px; */
+}
+</style>
