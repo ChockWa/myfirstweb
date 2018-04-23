4ba4290d4609ddcaa416002c9f11634b463c90d6
hezhuohua <hzh081215@qq.com>
2018-04-24 00:52:33 +0800

    aaaaaaaaaaaaaaaaaaaaaaaa

--- src/components/menu/header/header.js
+++ src/components/menu/header/header.js
@@ -1,27 +1,29 @@
-(function () {
-  var animenuToggle = document.querySelector('.animenu__toggle')
-  var animenuNav = document.querySelector('.animenu__nav')
-  var hasClass = function (elem, className) {
-    return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ')
-  }
-  var toggleClass = function (elem, className) {
-    var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' '
-    if (hasClass(elem, className)) {
-      while (newClass.indexOf(' ' + className + ' ') >= 0) {
-        newClass = newClass.replace(' ' + className + ' ', ' ')
-      }
-      elem.className = newClass.replace(/^\s+|\s+$/g, '')
-    } else {
-      elem.className += ' ' + className
+window.onload = function () {
+  (function () {
+    var animenuToggle = document.querySelector('.animenu__toggle')
+    var animenuNav = document.querySelector('.animenu__nav')
+    var hasClass = function (elem, className) {
+      return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ')
     }
-  }
-  var animenuToggleNav = function () {
-    toggleClass(animenuToggle, 'animenu__toggle--active')
-    toggleClass(animenuNav, 'animenu__nav--open')
-  }
-  if (!animenuToggle.addEventListener) {
-    animenuToggle.attachEvent('onclick', animenuToggleNav)
-  } else {
-    animenuToggle.addEventListener('click', animenuToggleNav)
-  }
-})()
+    var toggleClass = function (elem, className) {
+      var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' '
+      if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+          newClass = newClass.replace(' ' + className + ' ', ' ')
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '')
+      } else {
+        elem.className += ' ' + className
+      }
+    }
+    var animenuToggleNav = function () {
+      toggleClass(animenuToggle, 'animenu__toggle--active')
+      toggleClass(animenuNav, 'animenu__nav--open')
+    }
+    if (!animenuToggle.addEventListener) {
+      animenuToggle.attachEvent('onclick', animenuToggleNav)
+    } else {
+      animenuToggle.addEventListener('click', animenuToggleNav)
+    }
+  })()
+}
